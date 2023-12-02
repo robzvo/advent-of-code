add2541f5d50f8426acc2e38abf99b6fa0e581fa
robzvo <robzvo@users.noreply.github.com>
2023-12-01 22:09:38 -0500

    part 2 solution

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,3 +1,16 @@
+DICTIONARY = {
+    "one" : 1,
+    "two" : 2,
+    "three" : 3,
+    "four" : 4,
+    "five" : 5,
+    "six" : 6,
+    "seven" : 7,
+    "eight" : 8,
+    "nine" : 9,
+    "zero" : 0
+}
+
 # read game input
 input = None
 with open("input.txt") as f:
@@ -16,6 +29,13 @@ for line_index,line in enumerate(input):
     for char_index, char in enumerate(line):
         if char.isdigit():
             matches.append(char)
+        
+        # part 2 looking for word literals of numbers
+        for key,value in DICTIONARY.items():
+            
+            #literal exists at this position in the string
+            if line[char_index:].startswith(key):
+                matches.append(str(value))
     
     # at least one number was found
     if len(matches) > 0:
